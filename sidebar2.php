3a9e38b98c5ed85e54e059fa5522ea6cd7d9db8e
gerlyhomz <149936324+gerlyhomz@users.noreply.github.com>
2023-12-06 12:54:01 +0800

    Add files via upload
    gerly homdus

---
+++ sidebar2.php
@@ -0,0 +1,33 @@
+<!-- sidebar.php -->
+    <!-- Sidebar -->
+    <!-- sidebar2.php -->
+    <!-- Sidebar -->
+    <aside style="background-color: #555; color: #fff; width: 200px; height: 100vh; float: left;">
+        <section class="sidebar">
+        <ul class="sidebar-menu" style="list-style: none; padding: 0;">
+            <li class="treeview">
+                 <a href="student_dashboard.php" style="text-decoration: none; color: #fff; display: block; padding: 10px;">
+                    <i class="fa fa-dashboard"></i><span>Home</span>
+                </a>
+				
+                <a href="status.php" style="text-decoration: none; color: #fff; display: block; padding: 10px;">
+                    <i class="fa fa-dashboard"></i><span>Status</span>
+                </a>
+               
+                <a href="payment.php" style="text-decoration: none; color: #fff; display: block; padding: 10px;">
+                    <i class="fa fa-dashboard"></i><span>payment</span>
+                </a>
+				
+				<a href="logout.php" style="text-decoration: none; color: #fff; display: block; padding: 10px;">
+                    <i class="fa fa-dashboard"></i><span>Logout</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+    </aside>
+
+    <!-- Main Content -->
+    <main style="margin-left: 220px; padding: 20px;">
+	<aside class="main-sidebar">
+    
+</aside>
