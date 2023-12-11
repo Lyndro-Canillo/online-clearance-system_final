d4278d667bcc226f60938f6f853e488a5d57e484
Lyndro-Canillo <149144067+Lyndro-Canillo@users.noreply.github.com>
2023-12-11 13:10:04 +0800

    Delete canillo onlie system directory

--- canillo onlie system/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- footer.php -->
-    <!-- Footer -->
-    <footer style="background-color: maroon; color: white; padding: 5px; text-align: center; position: fixed; bottom: 0; width: 100%;">
-        <p>&copy; <?php echo date("Y"); ?> MSU-MSAT ONLINE CLEARANCE SYSTEM</p>
-    </footer>
-</body>
-</html>
